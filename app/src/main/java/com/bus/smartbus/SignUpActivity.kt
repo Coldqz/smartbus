f1d61ac24eaf5d5043d437ed6b96588da8537db2
Arsen Tsiurak <blizzcold1@gmail.com>
2020-06-18 01:47:12 +0300

    sign up beta2

--- app/src/main/java/com/bus/smartbus/SignUpActivity.kt
+++ app/src/main/java/com/bus/smartbus/SignUpActivity.kt
@@ -6,7 +6,9 @@ import android.content.Intent
 import android.util.Patterns
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlinx.android.synthetic.main.activity_sign_up.btn_sign_up
+import kotlinx.android.synthetic.main.activity_sign_up.tv_password
+import kotlinx.android.synthetic.main.activity_sign_up.tv_username
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -15,6 +17,7 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
         auth = FirebaseAuth.getInstance()
 
         btn_sign_up.setOnClickListener {
@@ -29,7 +32,7 @@ class SignUpActivity : AppCompatActivity() {
             tv_username.requestFocus()
             return
         }
-
+        auth
         if (!Patterns.EMAIL_ADDRESS.matcher(tv_username.text.toString()).matches()) {
             tv_username.error = "Please enter valid email"
             tv_username.requestFocus()
@@ -42,11 +45,19 @@ class SignUpActivity : AppCompatActivity() {
             return
         }
 
+        if (tv_password.text.toString().length < 6) {
+            tv_password.error = "Password must be at least 6 characters"
+            tv_password.requestFocus()
+            return
+        }
+
         auth.createUserWithEmailAndPassword(tv_username.text.toString(), tv_password.text.toString())
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     startActivity(Intent(this,LoginActivity::class.java))
                     finish()
+                    Toast.makeText(baseContext, "Successful, now you can sign in your account",
+                        Toast.LENGTH_SHORT).show()
                 } else {
                     Toast.makeText(baseContext, "Sign Up failed. Try again after some time.",
                         Toast.LENGTH_SHORT).show()
