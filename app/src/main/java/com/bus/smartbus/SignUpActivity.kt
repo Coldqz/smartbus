13ff7c69b97f721cf09a97785724be00c2901efb
Arsen Tsiurak <blizzcold1@gmail.com>
2020-06-18 00:54:02 +0300

    sign up beta1

--- app/src/main/java/com/bus/smartbus/SignUpActivity.kt
+++ app/src/main/java/com/bus/smartbus/SignUpActivity.kt
@@ -1,11 +1,10 @@
 package com.bus.smartbus
 
-import android.content.Intent
-
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
 import android.util.Patterns
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
