237e43043e31d6b79e16b7e8be57ebbaa6949a79
Arsen Tsiurak <blizzcold1@gmail.com>
2020-06-18 14:32:12 +0300

    login beta

--- app/src/main/java/com/bus/smartbus/LoginActivity.kt
+++ app/src/main/java/com/bus/smartbus/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.bus.smartbus
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.widget.Toast
+import android.util.Patterns
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
@@ -15,21 +17,76 @@ class LoginActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
+
         btn_sign_up.setOnClickListener{
             startActivity(Intent(this,SignUpActivity::class.java))
-            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            //overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
             finish()
+        }
+
+        btn_log_in.setOnClickListener {
+            doLogin()
         }
     }
 
     public override fun onStart() {
         super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         updateUI(currentUser)
     }
 
-    fun updateUI(currentUser:FirebaseUser?){
-
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            if(currentUser.isEmailVerified) {
+                startActivity(Intent(this,MainActivity::class.java))
+                //overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+                finish()
+            }else{
+                Toast.makeText(
+                    baseContext, "Please verify your email address.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
+
+    //Login function
+
+    private fun doLogin() {
+        if (tv_username.text.toString().isEmpty()) {
+            tv_username.error = "Please enter email"
+            tv_username.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(tv_username.text.toString()).matches()) {
+            tv_username.error = "Please enter valid email"
+            tv_username.requestFocus()
+            return
+        }
+
+        if (tv_password.text.toString().isEmpty()) {
+            tv_password.error = "Please enter password"
+            tv_password.requestFocus()
+            return
+        }
+
+        if (tv_password.text.toString().length < 6) {
+            tv_password.error = "Password must be at least 6 characters"
+            tv_password.requestFocus()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(tv_username.text.toString(), tv_password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+
+                    updateUI(null)
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/bus/smartbus/SignUpActivity.kt
+++ app/src/main/java/com/bus/smartbus/SignUpActivity.kt
@@ -6,9 +6,7 @@ import android.content.Intent
 import android.util.Patterns
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_sign_up.btn_sign_up
-import kotlinx.android.synthetic.main.activity_sign_up.tv_password
-import kotlinx.android.synthetic.main.activity_sign_up.tv_username
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -19,12 +17,13 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         auth = FirebaseAuth.getInstance()
-
         btn_sign_up.setOnClickListener {
             signUpUser()
         }
 
     }
+
+    //signUp function
 
     private fun signUpUser() {
         if (tv_username.text.toString().isEmpty()) {
@@ -32,7 +31,7 @@ class SignUpActivity : AppCompatActivity() {
             tv_username.requestFocus()
             return
         }
-        auth
+
         if (!Patterns.EMAIL_ADDRESS.matcher(tv_username.text.toString()).matches()) {
             tv_username.error = "Please enter valid email"
             tv_username.requestFocus()
@@ -54,13 +53,19 @@ class SignUpActivity : AppCompatActivity() {
         auth.createUserWithEmailAndPassword(tv_username.text.toString(), tv_password.text.toString())
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    startActivity(Intent(this,LoginActivity::class.java))
-                    finish()
-                    Toast.makeText(baseContext, "Successful, now you can sign in your account",
-                        Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                    user?.sendEmailVerification()
+                        ?.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                startActivity(Intent(this, LoginActivity::class.java))
+                                finish()
+                            }
+                        }
                 } else {
-                    Toast.makeText(baseContext, "Sign Up failed. Try again after some time.",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        baseContext, "Sign Up failed. Try again after some time.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
     }
